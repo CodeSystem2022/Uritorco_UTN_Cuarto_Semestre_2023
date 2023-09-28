90995e04ee5ccb917d653f463dd8b82740c27f9a
Luicha <iuriygomez@gmail.com>
2023-09-28 00:34:26 -0300

    Clase 6 Python

---
+++ Python/Clase06/tipo_str.py
@@ -0,0 +1,14 @@
+import math
+# Profundizado en el tipo String
+# Concatenación autoática en Python
+
+variable = ' Adios'
+mensaje = 'Hola ''Alumnos' + variable
+mensaje += ' , Terminamos'
+# print(mensaje)
+
+# Usamos la clase helppara ayuda o documentación (built-in)
+help(math.isnan)
+
+
+
