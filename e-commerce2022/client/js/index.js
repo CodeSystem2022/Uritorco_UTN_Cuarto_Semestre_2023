22aba3570c373344aec383cac3cb8c47d1e07715
Carlos Pereira <sillyhats.tv@gmail.com>
2023-10-02 20:18:39 -0300

    actualizo archivos del ecommerce
    actualizo archivos del ecommerce
    
    finalizado =)

--- e-commerce2022/client/js/index.js
+++ e-commerce2022/client/js/index.js
@@ -1,5 +1,5 @@
 const shopContent = document.getElementById("shopContent");
-const cart = [];//Este es nuestro carrito, un array vacio
+const cart = []; //este es nuestro carrito, un array vacío
 
 productos.forEach((product) =>{
     const content = document.createElement("div");
@@ -7,7 +7,7 @@ productos.forEach((product) =>{
     content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.productName}</h3>
-    <p class="price">${product.price} $</p>
+    <p>${product.price} $</p>
     `;
     shopContent.append(content);
 
@@ -17,13 +17,24 @@ productos.forEach((product) =>{
     content.append(buyButton);
 
     buyButton.addEventListener("click", ()=>{
-        createImageBitmap.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
-        })
-        console.log(cart)
-    })
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+        if (repeat) {
+            cart.map((prod) => {
+                if (prod.id === product.id){
+                    prod.quanty++;
+                    displayCartCounter();
+                }
+            });            
+        }else{
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            });
+            displayCartCounter();
+        }
+        //console.log(cart)
+    });
 });
